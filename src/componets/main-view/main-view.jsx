0092ea8006ec4e1fd3aa323f92d597da451c045c
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2023-12-14 13:16:57 -0700

    fetch from api and adding proptypes

--- src/componets/main-view/main-view.jsx
+++ src/componets/main-view/main-view.jsx
@@ -1,88 +1,57 @@
-import { useState } from "react";
-import { MovieCard } from "../main-view/movie-card.jsx";
-import { MovieView } from "../main-view/movie-view.jsx";
+import { useState, useEffect } from "react";
+import { MovieCard } from "../movie-card/movie-card.jsx";
+import { MovieView } from "../movie-view/movie-view.jsx";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Outsiders",
-      description:
-        "A teen gang in rural Oklahoma, the Greasers are perpetually at odds with the Socials, a rival group.",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5DkeAQqJbmkCB-260JOG6DE70syFT2U5IXxgbR9akZBK-TYxZ1bgtDHjQtbfLmsSnnJs&usqp=CAU",
-      genre: "Crime",
-      director: "Francis Ford Coppola"
-    },
-    {
-      id: 2,
-      title: "Dead Poets Society",
-      description:
-        "A new English teacher, John Keating (Robin Williams), is introduced to an all-boys preparatory school that is known for its ancient traditions and high standards",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgAOhZ6bapQHrvCFuHqlfcQ2XxJZ-NiVa6iWdId-DYdVRquzJV4-v2c1pFbE0yPOe2IfI&usqp=CAU",
-      genre: "Comedy",
-      director: "Peter Weir"
-    },
-    {
-      id: 3,
-      title: "Ghostbusters",
-      description:
-        "Three parapsychologists forced out of their university funding set up shop as a unique ghost removal service in New York City",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJgkhp8FTPHihILqnvzpzleGCfzUCoZYGuclQ7jS_4iQbqY-Aip2lB1u7XUtszYV9goJA&usqp=CAU",
-      genre: "Comedy",
-      director: "Ivan Reitman"
-    }
-  ]);
+    const [movies, setMovies] = useState([]);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+    useEffect(() => {
+        fetch("https://moviesapi-o4y1.onrender.com")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                const moviesFromApi = data.map((movie) => {
+                    return {
+                        _id: movie._id,
+                        Title: movie.Title,
+                        ImagePath: movie.ImagePath,
+                        Description: movie.Description,
+                        Year: movie.Year,
+                        Genre: {
+                            Name: movie.Genre.Name
+                        },
+                        Director: {
+                            Name: movie.Director.Name
+                        }
+                    };
+                });
+                setMovies(moviesFromApi);
+            });
+    }, []);
 
-  if (selectedMovie) {
-    return (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-    );
-  }
+    const [selectedMovie, setselectedMovie] = useState(null);
 
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-
-  return (
-    <div>
-      {movies.map((movie) => {
+    if (selectedMovie) {
         return (
-          <MovieCard
-            key={movie.id}
-            movieData={movie}
-            onMovieClick={(newSelectedMovie) =>
-              setSelectedMovie(newSelectedMovie)
-            }
-          />
+            <MovieView movie={selectedMovie} onBackClick={() => setselectedMovie(null)} />
         );
-      })}
-    </div>
-  );
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((movie) => (
+                    <MovieCard
+                    key={movie._id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setselectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))}
+        </div>
+    );
 };
-
-/* 
-import React from "react";
-
-export const MainView = () => {
-  return (
-    <>
-      <div>
-        <div>Eloquent JavaScript</div>
-        <div>Mastering JavaScript Functional Programming</div>
-        <div>JavaScript: The Good Parts</div>
-        <div>JavaScript: The Definitive Guide</div>
-        <div>The Road to React</div>
-      </div>
-      <button>Test</button>
-    </>
-  );
-}
-*/
--- src/componets/main-view/movie-card.jsx
+++ src/componets/main-view/movie-card.jsx
@@ -1,11 +1,22 @@
-export const MovieCard = ({ movieData, onMovieClick }) => {
+import PropTypes from "prop-types";
+
+export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <div
-        onClick={() => {
-          onMovieClick(movieData);
-        }}
-      >
-        {movieData.title}
-      </div>
+        <div
+            onClick={() => {
+                onMovieClick(movie);
+            }}
+        >
+            {movie.Title}
+        </div>
+        
     );
-  };
+};
+
+// define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string,        
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
