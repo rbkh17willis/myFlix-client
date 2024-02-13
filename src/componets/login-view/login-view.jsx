7eb9243f097fb4cb2393e140a9ebdfd8774a34ae
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2024-02-12 18:11:08 -0700

    boostrap and delete parcel cache

--- src/componets/login-view/login-view.jsx
+++ src/componets/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
@@ -35,28 +37,51 @@ export const LoginView = ({ onLoggedIn }) => {
           alert("Something went wrong");
         });
     }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
+   /* return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    ); */
+   return (
+    <Form onSubmit={handleSubmit}>
+    <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control 
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)} 
+        required
+        minLength="5"
         />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
+    </Form.Group>
+    <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control 
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+         />
+    </Form.Group>
+    <Button type="submit" className="mt-2">Submit</Button>
+  </Form>
+  ); 
 }
