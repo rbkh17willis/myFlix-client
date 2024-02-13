98f2ee05d3bfcc3b98ca5c1c3ae706ea0d15c3e8
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2024-02-13 15:25:26 -0700

    react-router-dom, render() menthod, profile and navigation views

--- src/componets/login-view/login-view.jsx
+++ src/componets/login-view/login-view.jsx
@@ -37,31 +37,9 @@ export const LoginView = ({ onLoggedIn }) => {
           alert("Something went wrong");
         });
     }
-   /* return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    ); */
+    
    return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} className="mt-5">
     <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
         <Form.Control 
--- src/componets/movie-view/movie-view.jsx
+++ src/componets/movie-view/movie-view.jsx
@@ -1,60 +1,84 @@
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import { MovieCard } from "../movie-card/movie-card";
 
-export const MovieView = ({ movie, onBackClick }) => {
-   return (
-      <Row className="my-5 justify-content-center">
-      <Col md={5} >
-          <img src={movie.ImagePath} alt="movie cover" className="img-fluid"/>
-      </Col>
-      <Col md={3}>
-          <div className="my-1">
-              <span className="h1">{movie.Title}</span>
-          </div>
-          <div className="my-1">
-              <span className="h6">Description: </span>
-              <span>{movie.Description}</span>
-          </div>
-          <div className="my-1">
-              <span className="h6">Director: </span>
-              <span>{movie.Director.Name}</span>
-          </div>
-          <div className="my-1">
-              <span className="h6">Genre: </span>
-              <span>{movie.Genre.Name}</span>
-          </div>
-          <Button onClick={onBackClick} variant="link">Back</Button>
-      </Col>
-  </Row>
+export const MovieView = ({ movies, removeFav, addFav}) => {
+
+    const { movieId } = useParams();
+    const movie = movies.find((movie) => movie._id === movieId);
+/*
+    // Similar Movies By Genre
+    const selectedMovie = movies.find((movie) => movie._id === movieId);
+    const similarMovies = movies.filter((movie) => {
+        return movie._id !== movieId && movie.Genre.Name === selectedMovie.Genre.Name;
+    });
+*/
+
+    // The User
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    // Debugging
+    console.log(user);
+
+    return (
+        <>
+            <Row className="my-5 justify-content-md-center">
+                <Col md={7} className="col-12">
+                    <img src={movie.ImagePath} alt="movie cover" className="mx-auto w-100" />
+                </Col>
+                <Col md={5} className="col-12">
+                    <div className="my-1">
+                        <span className="h1">{movie.Title}</span>
+                    </div>
+                    <div className="my-1">
+                        <span className="h6">Description: </span>
+                        <span>{movie.Description}</span>
+                    </div>
+                    <div className="my-1">
+                        <span className="h6">Director: </span>
+                        <span>{movie.Director.Name}</span>
+                    </div>
+                    <div className="my-1">
+                        <span className="h6">Genre: </span>
+                        <span>{movie.Genre.Name}</span>
+                    </div>
+                    <div className="my-1">
+                        <span className="h6">Year: </span>
+                        <span>{movie.Year}</span>
+                    </div>
+                    <div>
+                        {user.FavoriteMovies.includes(movie._id) ? (
+                            <Button className="my-2 me-2"on onClick={() => removeFav(movie._id)}>Remove from Favorite</Button>
+                        ) : (
+                            <Button className="my-2 me-2" onClick={() => addFav(movie._id)}>Add to Favorite</Button>
+                        )}
+                        </div>
+                    <Link to={`/`}>
+                        <Button className="my-2">Back</Button>
+                    </Link>
+                </Col>
+            </Row>
+            <h2>Similar Movies</h2>
+            <Row className="justify-content-center">
+                {
+                similarMovies.length !== 0 ?
+                similarMovies.slice(0,5).map((movie) => (
+                    <Col  sm={5} md={4} lg={3} xl={2} className="mx-2 my-3 col-6 similar-movies-img" key={movie._id}>
+                        <MovieCard
+                            movie={movie} 
+                            removeFav={removeFav} 
+                            addFav={addFav} 
+                            isFavorite={user.FavoriteMovies.includes(movie._id)}
+                        />
+                    </Col>
+                ))
+                : <Col>
+                <p>There are no similar Movies</p>
+                </Col>
+                }
+            </Row>
+        </>
     );
-    /* return (
-        <div>
-            <div>
-                <img src={movie.ImagePath} alt="movie cover"/>
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.Title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.Description}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.Director.Name}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.Genre.Name}</span>
-            </div>
-            <div>
-                <span>Year: </span>
-                <span>{movie.Year}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );*/
-  };
-
+};
